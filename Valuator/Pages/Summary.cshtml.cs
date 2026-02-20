6aefa3e25bfd971f633b38d8da44fef9a822f317
Maria Titova <mashababina857@gmail.com>
2026-02-20 16:44:04 +0300

    merged

--- Valuator/Pages/Summary.cshtml.cs
+++ Valuator/Pages/Summary.cshtml.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Valuator.Pages;
 public class SummaryModel : PageModel
 {
     private readonly ILogger<SummaryModel> _logger;
+    private readonly IConnectionMultiplexer _redis;
 
-    public SummaryModel(ILogger<SummaryModel> logger)
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redis)
     {
         _logger = logger;
+        _redis = redis;
     }
 
     public double Rank { get; set; }
@@ -22,7 +25,10 @@ public class SummaryModel : PageModel
     public void OnGet(string id)
     {
         _logger.LogDebug(id);
+        var db = _redis.GetDatabase();
 
+        Rank = (double)db.StringGet("RANK-" + id);
+        Similarity = (int)db.StringGet("SIMILARITY-" + id);
         // TODO: (pa1) проинициализировать свойства Rank и Similarity значениями из БД (Redis)
     }
 }
